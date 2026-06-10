74f71b8e8b691ff6a497cad657e649581a88fa87
Giira <euan.a.marsh@gmail.com>
2026-06-10 23:11:49 +0100

    basic heading retrieval - probably needs refactor

--- get_heading.go
+++ get_heading.go
@@ -5,4 +5,13 @@ import "strings"
 func getHeadingFromHTML(html string) string {
 	var out []string
 	out = strings.Split(html, "<h1>")
+	if len(out) == 1 {
+		out = strings.Split(out[0], "<h2>")
+		if len(out) == 1 {
+			return ""
+		}
+		out = strings.Split(out[1], "</h2>")
+	}
+	out = strings.Split(out[1], "</h1>")
+	return out[0]
 }
