03c15f2013577355efd4edf1eb4818e56888cc37
Giira <euan.a.marsh@gmail.com>
2026-06-13 23:24:32 +0100

    flesh out heading retrieval

--- get_heading.go
+++ get_heading.go
@@ -8,23 +8,23 @@ import (
 )
 
 func getHeadingFromHTML(html string) string {
-	var out []string
-	out = strings.Split(html, "<h1>")
-	if len(out) == 1 {
-		out = strings.Split(out[0], "<h2>")
-		if len(out) == 1 {
-			return ""
-		}
-		out = strings.Split(out[1], "</h2>")
-	}
-	out = strings.Split(out[1], "</h1>")
-
 	reader := strings.NewReader(html)
 	doc, err := goquery.NewDocumentFromReader(reader)
 	if err != nil {
-		log.Fatal("failed to prepare for goquery search: %v", err)
+		log.Fatalf("failed to prepare for goquery search: %v", err)
 	}
-	if doc.Find("<h1>") {
 
+	selection := doc.Find("h1").First()
+	heading := selection.Text()
+	if heading != "" {
+		return heading
 	}
+
+	selection = doc.Find("h2").First()
+	heading = selection.Text()
+	if heading != "" {
+		return heading
+	}
+
+	return ""
 }
