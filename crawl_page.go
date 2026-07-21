2ec217dfa989ce399f83f60e66a2f24bf0acb7b2
Giira <euan.a.marsh@gmail.com>
2026-07-21 13:33:39 +0100

    Accidentally built a ddos bot

---
+++ crawl_page.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+func crawlPage(rawBaseURL string, rawCurrentURL string, pages map[string]int) {
+	if !strings.HasPrefix(rawCurrentURL, rawBaseURL) {
+		return
+	}
+
+	normURL, err := normaliseURL(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("error in normaliseURL: %v", err)
+		return
+	}
+
+	i, ok := pages[normURL]
+	if !ok {
+		pages[normURL] = 1
+	} else {
+		pages[normURL] = i + 1
+	}
+
+	fmt.Printf("Crawling %v\n", normURL)
+	body, err := getHTML(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("error in getHTML: %v\n", err)
+		return
+	}
+
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		fmt.Printf("failed to parse url: %v\n", err)
+		return
+	}
+	urls, err := getURLsFromHTML(body, baseURL)
+	for _, url := range urls {
+		crawlPage(rawBaseURL, url, pages)
+	}
+}
--- main.go
+++ main.go
@@ -17,12 +17,11 @@ func main() {
 		os.Exit(1)
 	} else {
 		fmt.Printf("starting crawl of: %v\n", args[0])
-		html, err := getHTML(args[0])
-		if err != nil {
-			fmt.Print(err)
-			os.Exit(1)
+		pages := make(map[string]int)
+		crawlPage(args[0], args[0], pages)
+		fmt.Print("Pages crawled:\nPage: Visits\n")
+		for key, value := range pages {
+			fmt.Printf("%v: %v\n", key, value)
 		}
-		fmt.Print(html)
 	}
-
 }
